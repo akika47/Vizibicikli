0669a604f10639c56405902d4895a0a65f7aa773
Ákos Ö <akika02531@gmail.com>
2023-05-22 11:42:30 +0200

    előkészületek készen

---
+++ Kolcsonzes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqlgyak
+{
+	internal class Kolcsonzes
+	{
+		string nev;
+		char jazon;
+		int eOra;
+		int ePerc;
+		int vÓra;
+		int vPerc;
+		public Kolcsonzes(string csvSor)
+		{
+            var mezok = csvSor.Split(';');
+
+            this.nev = mezok[0];
+            this.jazon = mezok[1][0];
+            this.eOra = int.Parse(mezok[2]);
+            this.ePerc = int.Parse(mezok[3]);
+            this.vÓra = int.Parse(mezok[4]);
+            this.vPerc = int.Parse(mezok[5]);
+
+
+            Kolcsonzes beolvas = new Kolcsonzes(mezok[0], mezok[1][0], int.Parse(mezok[2]), int.Parse(mezok[3]), int.Parse(mezok[4]), int.Parse(mezok[5]));
+        }
+		public Kolcsonzes(string nev, char jazon, int eOra, int ePerc, int vÓra, int vPerc )
+		{
+			this.nev = nev;
+			this.jazon = jazon;
+			this.eOra = eOra;
+			this.ePerc = ePerc;
+            this.vÓra = vÓra;
+            this.vPerc = vPerc;
+
+		}
+
+		public string Nev { get => nev;}
+		public char Jazon { get => jazon;}
+		public int EOra { get => eOra; }
+		public int EPerc { get => ePerc; }
+        public int VÓra { get => vÓra; }
+        public int VPerc { get => vPerc; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Linq;
+﻿
 
-
-
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace sqlgyak
 {
     internal class Program
     {
+        List<Kolcsonzes> kolcsonzesek = new List<Kolcsonzes>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            StreamReader sr= new StreamReader("Kolcsonzesek.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                var mezok = sr.ReadLine().Split(';');
+                Kolcsonzes beolvas= new Kolcsonzes(mezok[0], mezok[1][0], int.Parse(mezok[2]), int.Parse(mezok[3]), int.Parse(mezok[4]), int.Parse(mezok[5]));
+            }
+            sr.Close();
+
+            List<Kolcsonzes> masik = File.ReadLines("kolcsonzes.txt").Skip(1).Select(x=>new Kolcsonzes(x)).ToList();
+
+
         }
     }
 }
